cbbffc5c15f7018ed51eee8db557461744d5ab0a
Gabriel Rocha <gabrielrochaas@gmail.com>
2022-05-24 22:22:53 -0300

    refactor(Input)!: convert `Input` to tsx file

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -8,9 +8,15 @@ import {
 import { useField } from '@unform/core';
 
 import { Container } from './styles';
+import { IconType } from 'react-icons';
 
-const Input = ({ name, icon: Icon, ...rest }) => {
-  const inputRef = useRef(null);
+interface InputProps {
+  name: string;
+  icon: IconType;
+}
+
+const Input = ({ name, icon: Icon, ...rest }: InputProps) => {
+  const inputRef = useRef<any | any[]>();
 
   const [isFocused, setIsFocused] = useState(false);
   const [isFilled, setIsFilled] = useState(false);
